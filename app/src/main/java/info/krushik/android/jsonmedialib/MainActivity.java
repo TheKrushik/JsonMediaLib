b4ff7c9954ef17a0e12b38faa6fb8c44c66daf6c
KrusH <krushik@ukr.net>
2016-09-14 10:49:33 +0300

    http

--- app/src/main/java/info/krushik/android/jsonmedialib/MainActivity.java
+++ app/src/main/java/info/krushik/android/jsonmedialib/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         btnHitVideo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new JSONTaskVideo().execute("http://fs39.www.ex.ua/get/05e8e8055a529012cdf14000014e70aa/272293084/Video.txt");
+                new JSONTaskVideo().execute("https://raw.githubusercontent.com/TheKrushik/JsonMediaLib/master/Video.txt");
             }
         });
 
@@ -49,10 +49,10 @@ public class MainActivity extends AppCompatActivity {
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        switch (item.getItemId()) {
 //            case R.id.action_down_video:
-//                new JSONTaskVideo().execute("http://fs39.www.ex.ua/get/05e8e8055a529012cdf14000014e70aa/272293084/Video.txt");
+//                new JSONTaskVideo().execute("https://raw.githubusercontent.com/TheKrushik/JsonMediaLib/master/Video.txt");
 //                return true;
 //            case R.id.action_down_book:
-//                new JSONTaskBook().execute("http://fs39.www.ex.ua/get/0d38999e21ef88a122ecbe44f073c052/272293104/Book.txt");
+//                new JSONTaskBook().execute("https://raw.githubusercontent.com/TheKrushik/JsonMediaLib/master/Book.txt");
 //                return true;
 //        }
 //        return super.onOptionsItemSelected(item);
